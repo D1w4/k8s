8ffbe86a2b30cd6d3372a4c27e441d2404448d7a
Diwa Hanugra Putra <diwahp@gmail.com>
2023-01-09 02:25:34 +0700

    Update dashboard-install.sh

--- dashboard-install.sh
+++ dashboard-install.sh
@@ -2,5 +2,5 @@
 #get dashboard
 echo kubernetes-dashboard is now installing, please wait ...
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml &
-sleep 5 &
+sleep 6
 echo kubernetes-dashboard installed.
