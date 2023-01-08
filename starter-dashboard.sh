0a1e0874b491c0bf823aa77a73840254eb92a86c
Diwa Hanugra Putra <diwahp@gmail.com>
2023-01-09 01:05:39 +0700

    Create starter-dashboard.sh
    This is script for running starter dashboard

---
+++ starter-dashboard.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
+kubectl port-forward -n kubernetes-dashboard service/Kubernetes-dashboard 8001:443 –address:’0.0.0.0’
