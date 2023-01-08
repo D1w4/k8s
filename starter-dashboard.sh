d5e433906f78238e5a6567738d2afda72229933b
Diwa Hanugra Putra <diwahp@gmail.com>
2023-01-09 01:33:42 +0700

    Create starter-dashboard.sh

---
+++ starter-dashboard.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+#get dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
+#running 127.0.0.1 to 0.0.0.0
+kubectl port-forward -n kubernetes-dashboard service/kubernetes-dashboard 8001:443 –-address='0.0.0.0'
