9a61c078139f63766744e029f7597efa41699f69
Diwa Hanugra Putra <diwahp@gmail.com>
2023-01-09 01:43:38 +0700

    Update starter-dashboard.sh

--- starter-dashboard.sh
+++ starter-dashboard.sh
@@ -2,4 +2,4 @@
 #get dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
 #running 127.0.0.1 to 0.0.0.0
-kubectl port-forward -n kubernetes-dashboard service/kubernetes-dashboard 8001:443 –-address='0.0.0.0'
+kubectl port-forward -n kubernetes-dashboard service/kubernetes-dashboard 8001:443 --address='0.0.0.0'
