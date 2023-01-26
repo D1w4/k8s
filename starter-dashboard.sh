c860ecf80a036e23283deff77396244ee2e1f88f
Diwa Hanugra Putra <diwahp@gmail.com>
2023-01-27 04:45:41 +0700

    Update starter-dashboard.sh

--- starter-dashboard.sh
+++ starter-dashboard.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 curl -S https://raw.githubusercontent.com/diwa-19/k8s/main/dashboard-install.sh | bash && sleep 1
-curl -S https://raw.githubusercontent.com/diwa-19/k8s/main/dashboard-run.sh | bash && sleep 1
+#curl -S https://raw.githubusercontent.com/diwa-19/k8s/main/dashboard-run.sh | bash && sleep 1
 echo All Done.
